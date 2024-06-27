5e7f1971881ec92e639bc5f613d7de609dc3e1b1
Rodrigodejesusfg <102862141+Rodrigodejesusfg@users.noreply.github.com>
2024-06-27 13:20:09 -0300

    Update chatbot-rfi.py

--- chatbot-rfi.py
+++ chatbot-rfi.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import google.generativeai as genai
+from google import generativeai as genai
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.decomposition import TruncatedSVD
